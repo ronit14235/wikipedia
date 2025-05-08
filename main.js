623b2bf98bcb1acaf7a9e9afc73f26457271d9de
ronit14235 <ronit14235@gmail.com>
2025-05-08 08:21:19 -0700

    added functionality: search wikipedia

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-function handleSubmit(event){
+async function handleSubmit(event){
     event.preventDefault();
     
     const inputValue = document.querySelector('.js-search-input').value;
     const searchQuery = inputValue.trim();
 
-    console.log(searchQuery);
+    try{
+        const results = await searchWikipedia(searchQuery);
+        console.log(results);
+    }
+    catch(err){
+        console.log(err);
+        alert('Failed to search Wikipedia');
+    }
+}
+
+async function searchWikipedia(searchQuery){
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    const response = await fetch(endpoint);
+    if(!response.ok){
+        throw Error(response.statusText);
+    }
+    const json = await response.json();
+    return json;
 }
 
 const form = document.querySelector('.js-search-form');
